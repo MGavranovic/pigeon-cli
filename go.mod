96fcff735b796b6070f18057fc02a3e1e2786847
MGavranovic <mgavranovic7@gmail.com>
2025-05-28 11:39:02 +0200

    Add go.mod / get current path

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MGavranovic/pigeon-cli
+
+go 1.24.1
--- main.go
+++ main.go
@@ -2,10 +2,33 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	test := 43
-	fmt.Println("Hello World!")
-	fmt.Printf("This is %d years old", test)
+	fmt.Println("Hello World!\nWelcome to pigeon-cli!")
+
+	dir, err := os.ReadDir("./")
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
+	}
+	for _, file := range dir {
+		fmt.Printf("File: %s\n", file.Name())
+	}
+
+	inFileSystem := true
+
+	for inFileSystem {
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Printf("Error accessing path: %s", err)
+		}
+		fmt.Printf("%s:", wd)
+
+		var input string
+		fmt.Scan(&input)
+		if input == "exit" {
+			os.Exit(0)
+		}
+	}
 }
