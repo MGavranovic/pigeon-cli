c018137988c324710a6bea948194c02d1db68c3a
MGavranovic <mgavranovic7@gmail.com>
2025-06-17 14:51:07 +0200

    Finish adding mv cmd

--- internal/cmd/mv.go
+++ internal/cmd/mv.go
@@ -21,26 +21,52 @@ func (c *MvCommand) Execute(args []string) error {
 	if len(args) < 2 {
 		return fmt.Errorf("pleace specify the file you want to move and the destination dir")
 	}
+	// 1. make a note of the original/current wd
+	curwd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("error getting the current working dir: %s", err)
+	}
 
+	// 2. open the file being moved
 	file, err := os.Open(args[0])
 	if err != nil {
-		return fmt.Errorf("error opening %s", file.Name())
+		return fmt.Errorf("error opening file: %s", err)
 	}
-	defer file.Close()
 
-	dest, err := os.Create(args[1])
+	// 3. change dir to the file destinationo
+	if err := os.Chdir(args[1]); err != nil {
+		file.Close()
+		return fmt.Errorf("error changing to '%s' dir: %s", args[1], err)
+	}
+
+	//4. create the destination file
+	newFile, err := os.Create(file.Name())
 	if err != nil {
-		return fmt.Errorf("errror creating %s", dest.Name())
+		file.Close()
+		return fmt.Errorf("errror creating file: %s", err)
 	}
-	defer dest.Close()
+	defer newFile.Close()
 
-	_, errCopy := io.Copy(dest, file)
+	// 5. copy the contents
+	_, errCopy := io.Copy(newFile, file)
 	if errCopy != nil {
-		return fmt.Errorf("error copying contents of %s", file.Name())
+		file.Close()
+		return fmt.Errorf("error copying contents of file: %s", errCopy)
 	}
 
-	if err := os.Remove(file.Name()); err != nil {
-		return fmt.Errorf("unable to move %s to %s", file.Name(), dest.Name())
+	// 6. close the original file
+	if err := file.Close(); err != nil {
+		return fmt.Errorf("unable to close the file: %s", err)
+	}
+
+	// 7. go back to the original wd
+	if err := os.Chdir(curwd); err != nil {
+		return fmt.Errorf("error going back to the original dir: %s", err)
+	}
+
+	// 8. remove the original file
+	if err := os.Remove(args[0]); err != nil {
+		return fmt.Errorf("unable to remove the original file: %s", err)
 	}
 	return nil
 }
