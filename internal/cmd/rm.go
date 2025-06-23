68cf401acc7792374cf507875d57e42fc8c57ed9
MGavranovic <mgavranovic7@gmail.com>
2025-06-23 10:02:40 +0200

    Add force flag for rm

--- internal/cmd/rm.go
+++ internal/cmd/rm.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
@@ -13,7 +14,7 @@ func (c *RmCommand) Name() string {
 }
 
 func (c *RmCommand) Description() string {
-	return "Command for moving the files"
+	return fmt.Sprintf("Command for moving the files\n%-10s - rm <file> <flags[--force]>\n", "syntax:")
 }
 
 func (c *RmCommand) Execute(args []string) (bool, error) {
@@ -22,24 +23,33 @@ func (c *RmCommand) Execute(args []string) (bool, error) {
 	}
 
 	path := args[0]
-	fmt.Printf("Are you sure you want to remove '%s'?\n", path)
-	fmt.Println("Y/N?")
-
-	var input string
-	fmt.Scan(&input)
-	input = strings.ToUpper(strings.TrimSpace(input))
-	switch input {
-	case "Y":
+	if slices.Contains(args, "--force") {
 		err := os.Remove(path)
 		if err != nil {
-			return true, fmt.Errorf("error removing the file: %s", err)
+			return false, fmt.Errorf("error removing the file: %s", err)
 		}
-		fmt.Printf(" - %s - file deleted successfully\n", path)
-		return true, nil
-	case "N":
-		return true, fmt.Errorf("you chose not to delete the file")
-	default:
-		fmt.Println("Please enter Y or N")
+		fmt.Printf(" - %s - file deleted successfully with '--force'\n", path)
+		return false, nil
+	} else {
+		fmt.Printf("Are you sure you want to remove '%s'?\n", path)
+		fmt.Println("Y/N?")
+
+		var input string
+		fmt.Scan(&input)
+		input = strings.ToUpper(strings.TrimSpace(input))
+		switch input {
+		case "Y":
+			err := os.Remove(path)
+			if err != nil {
+				return false, fmt.Errorf("error removing the file: %s", err)
+			}
+			fmt.Printf(" - %s - file deleted successfully\n", path)
+			return true, nil
+		case "N":
+			return true, fmt.Errorf("you chose not to delete the file")
+		default:
+			fmt.Println("Please enter Y or N")
+		}
 	}
 
 	return false, nil
